8d8a0934b985929e09d7e257cb1aa1410b856f6a
Ariel11003 <ariel@asiarice.com.tw>
2022-09-17 15:45:18 +0800

    HW1

---
+++ class1/hw/hw.py
@@ -0,0 +1,18 @@
+'''
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+整數int  :
+布林bool :
+字串str  :
+浮點數float:
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+'''
