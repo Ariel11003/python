6c650f21a119e7ab957a0d56823aa43007d855b3
Ariel11003 <ariel@asiarice.com.tw>
2022-10-01 15:29:24 +0800

    上傳cllass3

---
+++ class2/class2.py
@@ -0,0 +1,26 @@
+'''
+python
+'ppppko1'*10
+max(10,20,30) 
+len('2134567890qwertyuiopasdfghjkzxcvbnm') 
+type(9898898989898989.99989898989898)
+int('10101')
+float('110110')
+str('111010')
+bool('101010')
+print('10,101,2020')
+print('你叫什麼')
+a =input('請輸入')
+print('嗨')
+print(a)
+print(type(a))
+'''
+
+r =input('請輸入半徑')
+r=int(r)
+pi=3.14
+area=pi*r**2
+print(area)
+
+
+
