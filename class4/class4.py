9647077c3e6ac4fa4a2637e0ba004a80796efa70
Ariel11003 <ariel@asiarice.com.tw>
2022-10-15 15:19:32 +0800

    上傳

---
+++ class4/class4.py
@@ -0,0 +1,44 @@
+'''
+not False
+not True
+not not False
+
+Ture and Ture
+Ture and False
+False and Ture
+False and False
+
+Ture or Ture
+Ture or False
+False or Ture
+False or False
+'''
+'''
+password =input('請輸入密碼')
+if password =='0987654321':
+    print('您好')
+elif password=='11003':
+    print('歡迎雨涵')
+else:
+    print('再見')
+'''
+'''
+password =int(input('請輸入成績'))
+if password >=90:
+    print('A')
+elif password>=80:
+    print('B')
+elif password>=70:
+    print('C')
+elif password>=60:
+    print('D')
+elif password<=59:
+    print('E')
+else:
+    print('再見')
+'''
+password =int(input('請輸入數字'))
+if password %2==0:
+    print('偶')
+else:
+    print('奇')
