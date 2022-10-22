2699c88dc9e3b9794a774f223d82391e462db2e0
Ariel11003 <ariel@asiarice.com.tw>
2022-10-22 16:17:34 +0800

    上傳

--- class5/hw/hw5(1).py
+++ class5/hw/hw5(1).py
@@ -4,3 +4,14 @@ t0_turtle_stamp.jpg
 提示：
 turtle.home()是讓烏龜回到原點的指令
 """
+import turtle 
+turtle.penup()
+for a in range(0,360,45):
+    turtle.right(a)
+    turtle.forward(50)
+    turtle.stamp()
+    turtle.home()
+turtle.done()
+
+
+
--- class5/hw/hw5.py
+++ class5/hw/hw5.py
@@ -7,3 +7,14 @@ turtle.clear() # 清空畫面指令
 import time  # 匯入time模組
 time.sleep(1) # 延遲1秒
 """
+import turtle
+import time
+turtle.speed(0)
+for a in range(0,360,6):
+    turtle.right(a)
+    turtle.forward(50)
+    time.sleep(1)
+    turtle.home()
+    turtle.clear()
+turtle.done()
+
